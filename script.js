59e4fdf7c2e9b2c55c1aecbc8c9f78c3a67d84f6
Michael Ramos <michaelaramos182@gmail.com>
2022-02-24 23:01:42 -0600

    Cleaned up and formatted code.

--- script.js
+++ script.js
@@ -3,37 +3,123 @@ let generateBtn = document.querySelector("#generate");
 
 // write password function
 function writePassword() {
-let password = generatePassword();
-let passwordText = document.querySelector("#password");
-passwordText.value = password;
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
 
 // variable arrays for password characters
-let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
+let upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
-
+let lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let special = [
+  "@",
+  "%",
+  "+",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
 
 // Setting functions for generator questions
 
-function generatePassword()  {
+function generatePassword() {
   let randomPass = "";
   let passArray = [];
-  let passLength = parseInt(prompt("Enter the amount of characters between 8-128."));
+
+  // Using parseInt here to take the string entered value and turn it into an integer
+
+  let passLength = parseInt(
+    prompt("Enter the amount of characters between 8-128.")
+  );
 
   if (passLength > 128 || passLength < 8) {
     return alert("Invalid selection. Enter correct character amount");
   }
 
-  let passLowerCase = confirm("Do you want lower case characters in your password?");
+  // binary questions for character selection
+
+  let passLowerCase = confirm(
+    "Do you want lower case characters in your password?"
+  );
 
   if (passLowerCase === true) {
     passArray = passArray.concat(lowerCase);
   }
 
-  let passUpperCase = confirm("Do you want upper case characters in your password?");
+  let passUpperCase = confirm(
+    "Do you want upper case characters in your password?"
+  );
 
   if (passUpperCase === true) {
     passArray = passArray.concat(upperCase);
@@ -45,17 +131,20 @@ function generatePassword()  {
     passArray = passArray.concat(numbers);
   }
 
-  let passSpecialChar = confirm("Do you want special characters in your password?");
+  let passSpecialChar = confirm(
+    "Do you want special characters in your password?"
+  );
 
   if (passSpecialChar === true) {
     passArray = passArray.concat(special);
   }
 
-  for (let i=0; i < passLength; i++){
-    randomPass += passArray[Math.floor(Math.random() * (passArray.length))];
+  // for loop to randomize character selection to generate random password
+
+  for (let i = 0; i < passLength; i++) {
+    randomPass += passArray[Math.floor(Math.random() * passArray.length)];
   }
   return randomPass;
-
 }
 
 // Add event listener to generate button
