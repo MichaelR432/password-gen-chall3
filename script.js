f9b43c13ed65c54265bf08190bf531307e56acf8
Michael Ramos <michaelaramos182@gmail.com>
2022-02-24 22:52:06 -0600

    Added in function for character selection, something isn't working yet. I think it has to do with my scope.

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
 // Assignment code here
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+
+// write password function
 function writePassword() {
-var password = generatePassword();
-var passwordText = document.querySelector("#password");
+let password = generatePassword();
+let passwordText = document.querySelector("#password");
 passwordText.value = password;
 }
 
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
+// variable arrays for password characters
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
 ];
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// Setting functions for generator questions
+
+function generatePassword()  {
+  let randomPass = "";
+  let passArray = [];
+  let passLength = parseInt(prompt("Enter the amount of characters between 8-128."));
+
+  if (passLength > 128 || passLength < 8) {
+    return alert("Invalid selection. Enter correct character amount");
+  }
+
+  let passLowerCase = confirm("Do you want lower case characters in your password?");
+
+  if (passLowerCase === true) {
+    passArray = passArray.concat(lowerCase);
+  }
+
+  let passUpperCase = confirm("Do you want upper case characters in your password?");
+
+  if (passUpperCase === true) {
+    passArray = passArray.concat(upperCase);
+  }
+
+  let passNumbers = confirm("Do you want numbers in your password?");
+
+  if (passNumbers === true) {
+    passArray = passArray.concat(numbers);
+  }
+
+  let passSpecialChar = confirm("Do you want special characters in your password?");
+
+  if (passSpecialChar === true) {
+    passArray = passArray.concat(special);
+  }
+
+  for (let i=0 < passLength; i++;){
+    randomPass += passArray[Math.floor(Math.random() * (passArray.length))];
+  }
+  return randomPass;
+
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
